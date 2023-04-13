0a876d353d4adcdcea158ef968f8ea03840e54e4
Max <thewildstar.m@gmail.com>
2023-04-13 23:12:19 +0300

    add header

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -1,3 +1,11 @@
 export default () => {
-	console.log("Я тестовый лог");
+	const burgerBtn = document.getElementById("burger");
+
+	burgerBtn.addEventListener("click", () => {
+		const headerBody = document.querySelector(".header__body");
+		const page = document.body;
+
+		headerBody.classList.toggle("open");
+		page.classList.toggle("page_lock");
+	});
 };
